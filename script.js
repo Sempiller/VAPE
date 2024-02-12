9f0803bcb14ea0da49640e1cbe68c606d50adfdc
Doruk <110603660+Sempiller@users.noreply.github.com>
2024-02-12 20:01:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ function setBackground() {
 
     closePopup();
 }
+
+function setBackground() {
+    var fileInput = document.getElementById('image-upload');
+    var file = fileInput.files[0];
+
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.body.style.backgroundImage = 'url(' + e.target.result + ')';
+            
+            // localStorage kullanarak resmi sakla
+            localStorage.setItem('backgroundImage', e.target.result);
+        };
+        reader.readAsDataURL(file);
+    }
+
+    closePopup();
+}
+
+// Sayfa yüklendiğinde localStorage'da saklanan arka plan resmini kontrol et
+window.onload = function() {
+    var storedBackgroundImage = localStorage.getItem('backgroundImage');
+    if (storedBackgroundImage) {
+        document.body.style.backgroundImage = 'url(' + storedBackgroundImage + ')';
+    }
+};
+
+function resetBackgroundColor() {
+    document.body.style.backgroundImage = 'none';
+    document.body.style.backgroundColor = 'black'; // Arka plan rengini siyah yap
+    localStorage.removeItem('backgroundImage');
+}
+
+
