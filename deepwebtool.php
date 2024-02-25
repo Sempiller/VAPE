f643ad641b3dbac4f45ac7608d042921e03e56e4
Doruk <110603660+Sempiller@users.noreply.github.com>
2024-02-25 13:45:19 +0300

    Create deepwebtool.php

---
+++ deepwebtool.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>deepwebtools</title>
+</head>
+<body>
+ 
+    <h1>VAPE DeepDevTools</h1>
+    <a>Powered by MediaFire.</a>
+    <br><br>
+    <h3>VAPE Browser</h3>
+    <form action="https://sempiller.github.io/VAPE/">
+        <input type="submit" value="Go to Browser"/>
+    </form>
+
+    <br><br>
+    <h3>VAPE Javascript Executor</h3>
+    <form action="https://sempiller.github.io/VAPE/jawexec.html">
+        <input type="submit" value="Go to Executor"/>
+    </form>
+
+    <br><br>
+    <h3>Sasting DDOS Tool</h3>
+    <form action="https://download941.mediafire.com/y8fybnrn6plgh287Co6eh51_DQF6SMxfNiOJT-FV0OP0HiJsyh2AsHMfRX8-BBCYeeaOVzYh_vN18GbodTDlDbKl8ZaI4nmakv_4Qy78-dHrpmrg4F0qEs5u2-zJvPKJ_OGhZjjFnMItZzg4lotj3mh_U6KE84XBJn8hV6x24bki0w/f0do8ear8v5hojx/ddos.exe">
+        <input type="submit" value="Download DDOS Tool"/>
+    </form>
+
+    <br><br>
+    <h3>MineTool</h3>
+    <form action="https://download1334.mediafire.com/jz9qygd5s0pgZZJxiC88PzTIDebJXivbARYSVK2DYvGCsqMVgte-tO1EeC79CxJTfTQoDwuMNn_3zV0l4VPtHnXwiffb_IxbS40mVPIjsszpWCbUa0LToNu0BSkRmrElddKEhdG5tDtPjDYAnBgC2FqFlknxztzr6Pp0eXvO-4_ChQ/qek1yuk6mzvsb3d/mining.exe">
+        <input type="submit" value="Go to Bitcoin Miner"/>
+    </form>
+
+</body>
+</html>
